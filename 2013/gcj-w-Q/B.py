72ec7f209802597d7b07f50125e1566ea0826f6e
Ahmad Medhat Othman <amedhat3@gmail.com>
2013-04-14 00:41:15 +0200

    solving B larg

--- 2013/gcj-w-Q/B.py
+++ 2013/gcj-w-Q/B.py
@@ -10,7 +10,8 @@ amedhat.cs@gmail.com
 '''
 
 def main():
-	fin = open('B-small-attempt0.in', 'r')
+	#fin = open('B-small-attempt0.in', 'r')
+	fin = open('B-large.in', 'r')
 	fout = open('B.out','w')
 	T = long(fin.readline())
 	for tc in range(1,T+1):	
@@ -19,51 +20,59 @@ def main():
 		for i in range(N):
 			data.append(map(int,fin.readline().split()))
 		
-		tcount = 0
-		for i in range(N):
-			for j in range(M):
-				if data[i][j] == 1:
-					tcount += 1
-		#print 'tcount' , tcount
-		
-		for i in range(N):
-			rem = 0
-			rcount = 0
-			for j in range(M):
-				if data[i][j] == 1:
-					rem += 1
-				if data[i][j] <= 1:
-					rcount +=1
-			#print 'rcount ' ,rcount
-			#print 'rem ' ,rem
-			if rcount >= M:
-				tcount-= rem
-				for j in range(M):
-					if data[i][j] == 1:
-						data[i][j] -= 1
-
-		for j in range(M):
-			rem = 0
-			ccount = 0
+		f = True
+		for ind in range(1,max(max(data))):
+			tcount = 0
 			for i in range(N):
-				if data[i][j] == 1:
-					rem += 1
-				if data[i][j] <= 1:
-					ccount +=1
+				for j in range(M):
+					if data[i][j] == ind:
+						tcount += 1
+			#print 'tcount' , tcount
 			
-			#print 'ccount ' ,ccount
-			#print 'rem ' ,rem
-			if ccount >= N:
-				tcount -= rem
-				for i in range(N):
-					if data[i][j] == 1:
-						data[i][j] -= 1
+			for i in range(N):
+				rem = 0
+				rcount = 0
+				for j in range(M):
+					if data[i][j] == ind:
+						rem += 1
+					if data[i][j] <= ind:
+						rcount +=1
+				#print 'rcount ' ,rcount
+				#print 'rem ' ,rem
+				if rcount >= M:
+					tcount -= rem
+					for j in range(M):
+						if data[i][j] == ind:
+							data[i][j] -= ind
+							#if data[i][j] < 0:
+							#	data[i][j] = 0
 
-		#print 'tcount' , tcount
-		if tcount > 0:
-			res = 'NO'
-		else:
+			for j in range(M):
+				rem = 0
+				ccount = 0
+				for i in range(N):
+					if data[i][j] == ind:
+						rem += 1
+					if data[i][j] <= ind:
+						ccount +=1
+				
+				#print 'ccount ' ,ccount
+				#print 'rem ' ,rem
+				if ccount >= N:
+					tcount -= rem
+					for i in range(N):
+						if data[i][j] == ind:
+							data[i][j] -= ind
+
+			#print 'tcount' , tcount
+			if tcount > 0:
+				f = False
+				break
+		
+		if f:
 			res = 'YES'
+		else:
+			res = 'NO'
 
 		fout.write("Case #" + str(tc) + ": " + res +"\n")
 
